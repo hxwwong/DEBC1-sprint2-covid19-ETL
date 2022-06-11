4ac5cf0658f861fefffbc25807ae1b94b747c5df
bonojoves <bonojoves@gmail.com>
2022-06-11 05:26:47 +0000

    edited vaccine_scraper.sh to overwrite instead of creating new file

--- vaccine_scraper.sh
+++ vaccine_scraper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O vaccination_$(date +%d-%m-%Y).csv https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/Philippines.csv
+wget -O vaccination.csv https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/Philippines.csv
